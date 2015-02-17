a5728d2e4f01e45ccb27fe68f4e4131afe320a04
Vladimir Ilyin <oivt@ya.ru>
2015-02-17 19:45:09 +0300

    Initial commit

---
+++ src/ru/samsung/itschool/bdgame/HoFActivity.java
@@ -0,0 +1,29 @@
+package ru.samsung.itschool.bdgame;
+
+import java.util.ArrayList;
+
+import ru.samsung.itschool.mydraw.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HoFActivity extends Activity {
+
+	private DBManager dbManager;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_ho_f);
+		dbManager = DBManager.getInstance(this);
+		
+		TextView restv = (TextView)this.findViewById(R.id.results);
+		ArrayList<Result> results = dbManager.getAllResults();
+		String resStr = "";
+		for (Result res : results)
+		{
+			resStr += res.name + ": " + res.score + "\n";
+		}	
+		restv.setText(resStr);
+	}
+}
---
+++ src/ru/samsung/itschool/bdgame/Result.java
@@ -0,0 +1,11 @@
+package ru.samsung.itschool.bdgame;
+
+public class Result {
+        String name;
+        int score;
+        Result(String name, int score)
+        {
+        	this.name = name;
+        	this.score = score;
+        }
+}
